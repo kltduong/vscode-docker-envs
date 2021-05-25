2921efa71436f09669673791c6fe787d9213b508
Duong Trieu <kltduong@gmail.com>
2021-05-25 10:48:04 +0700

    Added start.sh file

---
+++ start.sh
@@ -0,0 +1,12 @@
+cp .cred/.ssh/* ~/.ssh/
+cp -r .cred/.aws ~/.aws/
+
+git config --global user.name "Duong Trieu" \
+    && git config --global user.email "kltduong@gmail.com" \
+    &&  git config --global core.editor "vim"
+
+export SSH_KEY=id_ed25519
+eval "$(ssh-agent -s)"
+chmod 400 ~/.ssh/${SSH_KEY}
+ssh-add ~/.ssh/${SSH_KEY}
+ssh -T git@github.com
